0cfebe721a75445aa271a60e0401493424c7e1d8
FlorenciaPereyra <pereyraramosflorenciacamila@gmail.com>
2023-11-02 10:27:09 -0300

    agregado de carpetas para DB.

--- backend/server.ts
+++ backend/server.ts
@@ -1,10 +1,39 @@
-import express, { Express, Application } from 'express'; 
+import express, { Request, Response } from 'express';
+
+const app = express();
+const port: number = 3000;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello World');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port http://localhost:${port}`);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* import express, { Express, Application } from 'express'; 
 import morgan from 'morgan';
 
 
 
 //routes
-import usuarioRoutes from './routes/usuario.routes';
+import usuarioRoutes from './app/routes/usuario.routes';
 const app: Express = express();
 
 
@@ -38,4 +67,4 @@ export class App{
     }
 }
 
-export default app;
+export default app; */
