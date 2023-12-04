79c10328172d2186a152b4d1546a8efeacb760b7
FlorenciaPereyra <pereyraramosflorenciacamila@gmail.com>
2023-12-04 18:08:04 -0300

    db comentada

--- backend/app/db/Dump20231128.sql
+++ backend/app/db/Dump20231128.sql
@@ -1,4 +1,4 @@
--- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+/*-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: triviator
 -- ------------------------------------------------------
@@ -106,3 +106,4 @@ UNLOCK TABLES;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
 -- Dump completed on 2023-11-28 10:55:05
+*/
--- backend/javascript/server.js
+++ backend/javascript/server.js
@@ -53,9 +53,8 @@ app.post('/api/registro', (req, res) => __awaiter(void 0, void 0, void 0, functi
             res.status(400).json({ mensaje: 'El usuario o correo ya están registrados' });
             return;
         }
-        const contraseñaValida = contraseña !== undefined ? contraseña : null;
         // Insertar el nuevo usuario en la base de datos
-        const [result] = yield connection.execute('INSERT INTO usuarios (username, correo, contraseña) VALUES (?, ?, ?)', [username, correo, contraseñaValida]);
+        const [result] = yield connection.execute('INSERT INTO usuarios (username, correo, contraseña) VALUES (?, ?, ?)', [username, correo, contraseña]);
         connection.release();
         // Verificar si la inserción fue exitosa
         if (result && 'insertId' in result) {
