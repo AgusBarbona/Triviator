e75cf0c8586ccdcc80cdc9a6d0e21a026ce537a9
Aixa Flores <aixafloressantillan@gmail.com>
2023-12-03 21:22:28 -0300

    actualiza tipografias

--- frontend/triviator-front/src/pages/Roulette/RuletaCategoria.tsx
+++ frontend/triviator-front/src/pages/Roulette/RuletaCategoria.tsx
@@ -1,14 +1,20 @@
 import { Ruleta } from "../../components/ruleta/ruleta";
+import Header  from "../../components/header/Header";
+import  Sorprendido  from "../../../public/recursos/sorprendido.png";
 import "../Roulette/_roulette_category.scss"
+
 
 export const RuletaCategoria = () => {
     return (
+      <div>
+        <Header/>
       <div className="Categoria">
        
         <div className="ruleta-container">
           
          <Ruleta/>
-       
+        <img src={ Sorprendido } alt="popcorn-sorprendido" className="sorprendido"/>
+      </div>
       </div>
       </div>
     );
