4bcf99e317c85fc41966b001f8db5fa336c508f8
Aixa Flores <aixafloressantillan@gmail.com>
2023-12-06 11:48:25 -0300

    agrega fondo dinamico a ruleta

--- frontend/triviator-front/src/pages/Roulette/RuletaCategoria.tsx
+++ frontend/triviator-front/src/pages/Roulette/RuletaCategoria.tsx
@@ -2,21 +2,42 @@ import { Ruleta } from "../../components/ruleta/ruleta";
 import Header from "../../components/header/Header";
 import Sorprendido from "../../../public/recursos/sorprendido.png";
 import "../Roulette/_roulette_category.scss";
-import Group from "../../../public/recursos/Group.png"
-import  Wave  from "../../../public/recursos/wave.svg"
+import Group from "../../../public/recursos/Group.png";
 
 export const RuletaCategoria = () => {
   return (
-    <div>
-      <Header />
-      <div className="Categoria">
-      <img src={ Wave } className="ondas"/>
+    <div className="Categoria">
+      <div className="background">
+        <Header />
         <div className="ruleta-container">
           <Ruleta />
-          <img src={ Sorprendido } alt="popcorn-sorprendido" className="sorprendido"/>
-          <img src={Group} className="group"/>
-
+          <img
+            src={Sorprendido}
+            alt="popcorn-sorprendido"
+            className="sorprendido"
+          />
+          <img src={Group} className="group" />
         </div>
+
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
       </div>
     </div>
   );
