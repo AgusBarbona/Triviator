5053bff8103883a6001b2f25f28d8931f1c2ad62
AgusBarbona <agusmua3110@gmail.com>
2023-12-07 10:04:01 -0300

    basta ya

--- backend/server.ts
+++ backend/server.ts
@@ -220,18 +220,11 @@ app.post('/api/verificar-respuesta', async (req: Request, res: Response) => {
     const respuestaCorrecta = (respuesta as RowDataPacket[])[0]?.respuesta_correcta;
 
     if (opcionSeleccionada === respuestaCorrecta) {
-      
       await connection.query(
         'UPDATE users SET points = points + 10 WHERE username = ?', 
         [username]
       );
     
-      res.status(200).json({ mensaje: 'Respuesta correcta. Sumar 10 puntos.', esCorrecta: true });
-    } else {
-      res.status(200).json({ mensaje: 'Respuesta incorrecta. No se suman puntos.', esCorrecta: false });
-    }
-    
-
       res.status(200).json({ mensaje: 'Respuesta correcta. Sumar 10 puntos.', esCorrecta: true });
     } else {
       res.status(200).json({ mensaje: 'Respuesta incorrecta. No se suman puntos.', esCorrecta: false });
