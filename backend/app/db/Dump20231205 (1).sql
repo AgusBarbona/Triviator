3ed18f919c0a17c1c8188903360284b29e70b0ca
Paula Cizmadija <cizmadijapaula@gmail.com>
2023-12-12 23:49:52 -0300

    feat: image url in db

--- backend/app/db/Dump20231205 (1).sql
+++ backend/app/db/Dump20231205 (1).sql
@@ -1,3 +1,5 @@
+/*
+
 CREATE DATABASE  IF NOT EXISTS `triviator` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `triviator`;
 -- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
@@ -118,3 +120,5 @@ UNLOCK TABLES;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
 -- Dump completed on 2023-12-05 11:12:35
+
+*/
